0c9c035719db0bcbcc1a86744c868af5d81c83a0
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-21 16:10:49 -0700

    Corrected spec file to pass with pseudo classical

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,17 +6,17 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
-    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(blinkyDancer._$node).to.be.an.instanceof(jQuery);
   });
 
   it("should have a step function that makes its node blink", function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');
+    sinon.spy(blinkyDancer._$node, 'toggle');
     blinkyDancer.step();
-    expect(blinkyDancer.$node.toggle.called).to.be.true;
+    expect(blinkyDancer._$node.toggle.called).to.be.true;
   });
 
   describe("dance", function(){
@@ -24,7 +24,7 @@ describe("blinkyDancer", function() {
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-      clock.tick(timeBetweenSteps);
+      // clock.tick(timeBetweenSteps);
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
