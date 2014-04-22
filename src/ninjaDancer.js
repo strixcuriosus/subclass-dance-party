28ba0fe676155b1b82cdc09f11b7dad426b27f25
Hack Reactor Students <strixcuriosus@gmail.com>
2014-04-21 20:17:22 -0700

    Added Ninjs and twirly stars

--- src/ninjaDancer.js
+++ src/ninjaDancer.js
@@ -1,6 +1,29 @@
 var NinjaDancer = function (top, left, timeBetweenSteps ) {
   Dancer.call(this, top, left, timeBetweenSteps);
+  this._$node.addClass("ninja");
+  this._top = top;
+  this._left = left;
+
 };
 
 NinjaDancer.prototype = Object.create(Dancer.prototype);
 NinjaDancer.prototype.constructor = NinjaDancer;
+NinjaDancer.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  var self = this;
+  var displayNinjaPic = function(){
+
+  };
+  var expand = {height: "20px", width: "20px", "border-radius": "20px"};
+  var shrink = {height: "0px", width: "0px", "border-radius": "10px"};
+
+  this._$node.animate(expand, 500).addClass("ninjaPic").animate(shrink, 500).fadeOut("slow", function () {
+    self._top  = (self._top + 200) % $("body").height();
+    self._left = (self._left + 20) % $("body").width();;
+    // self._$node.;
+    self.setPosition(
+      self._top,
+      self._left);
+    self._$node.fadeIn("slow");
+  });
+};
--- src/twirlyDancer.js
+++ src/twirlyDancer.js
@@ -1,6 +1,19 @@
 var TwirlyDancer = function (top, left, timeBetweenSteps) {
+  timeBetweenSteps = 50;
+  this._color = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),
+   Math.floor(Math.random() * 255)];
+  this._degrees = 180;
   Dancer.call(this, top, left, timeBetweenSteps);
+  this._$node.addClass("twirly");
+
 };
 
 TwirlyDancer.prototype = Object.create(Dancer.prototype);
 TwirlyDancer.prototype.constructor = TwirlyDancer;
+TwirlyDancer.prototype.step = function () {
+  Dancer.prototype.step.call(this);
+  this._$node.css("color", "rgb(" + this._color.join(",") + ")" );
+  this._$node.css("transform", "rotate(" + this._degrees + "deg)");
+  this._degrees = (1 + this._degrees) % 360;
+  // this._$node.rotate(5);
+};
